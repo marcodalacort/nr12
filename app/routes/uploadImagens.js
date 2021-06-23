e0c5fbea3038e78bc93c13179dd9934ab75bc828
marcodalacort <marcodalacort@gmail.com>
2021-06-23 16:19:59 -0300

    atualizado

--- app/routes/uploadImagens.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app){
-	var controller = app.controllers.uploadImagens;
-
-	// Utilizando sistema de rotas
-	app.route('/upload')
-		.post(controller.upload);
-};
--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-var app = require('./config/express')();
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Nr12 Server escutando porta ' + app.get('port'));
-});
